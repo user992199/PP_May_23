0696ec550d059bddaa0aefd3bdc8ac0e4ffcb514
user992199 <109051881+user992199@users.noreply.github.com>
2023-05-25 12:09:33 +0530

    Hello World

---
+++ 20-05/data_type_mod.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	signed int a;
+	a = -10;
+	cout<<a<<endl;
+
+	short int i = 1000000000;
+	cout<<i<<endl;
+
+	long long j = 800000000000000000;
+	cout<<j<<endl;
+
+	long double d = 36458.1283751823576;
+	cout<<d<<endl;
+	return 0;
+}
---
+++ 20-05/variables.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+	// Single Line Comment 
+	/*
+		Multi Line Comment
+	*/
+	// syntax for varible creation/declaration
+	// datatype identifier;
+	int a, b, c;
+	a = 10;
+	b = 5;
+	c = a + b;
+	cout << a << " " << b << " " << c << endl;
+	cout<< b <<endl;
+	cout<< c <<endl;
+
+	char ch;
+	ch = 126;
+	cout<<ch<<endl;
+
+	// int a;
+	// cout<<a<<endl;
+	// a = 10;
+	// cout<<a<<endl;
+
+	// int b = 10; // variable initialization
+	// cout<<b<<endl;
+
+	/* End of Program */return 0;
+}
